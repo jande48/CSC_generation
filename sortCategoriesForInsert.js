c5f12972c529cd3b9d350464099206a81a5de30d
Shawn Inder <shawninder@gmail.com>
2021-02-12 11:00:04 -0500

    fix indentation to help with readability

--- sortCategoriesForInsert.js
+++ sortCategoriesForInsert.js
@@ -5,28 +5,28 @@
 module.exports = function sortCategoriesForInsert (inputJson) {
 
   var arr = inputJson
-  // Since this data is hierarchial, it seems that one way to efficiently solve it is to create a hash array that groups objects with the same parent id. 
+  // Since this data is hierarchial, it seems that one way to efficiently solve it is to create a hash array that groups objects with the same parent id.
   var hashArr = {};
   for (var i = 0; i < arr.length; i++) {
       if (hashArr[arr[i].parent_id] == null) hashArr[arr[i].parent_id] = [];
       hashArr[arr[i].parent_id].push(arr[i]);
   }
-  
-  // Once the data is grouped by parent, a recursive function is called to push all of the children to a 'result' array once their parents have aleady been added. 
+
+  // Once the data is grouped by parent, a recursive function is called to push all of the children to a 'result' array once their parents have aleady been added.
   function hierarhySort(hashArr, key, result) {
 
-  // check to see if there are any children 
-  if (hashArr[key] == undefined) return;
-  
-  // separate out the array that have the same parents  
-  var arrOfParents = hashArr[key];
-  for (var i=0; i<arrOfParents.length; i++){
-    result.push(arrOfParents[i]);
-    hierarhySort(hashArr, arrOfParents[i]["id"], result)
+    // check to see if there are any children
+    if (hashArr[key] == undefined) return;
+
+    // separate out the array that have the same parents
+    var arrOfParents = hashArr[key];
+    for (var i=0; i<arrOfParents.length; i++){
+      result.push(arrOfParents[i]);
+      hierarhySort(hashArr, arrOfParents[i]["id"], result)
+    }
+
+    return result
   }
-    
-  return result
-}
-    var properJsonOutput = hierarhySort(hashArr, null, []);
-    return properJsonOutput
+  var properJsonOutput = hierarhySort(hashArr, null, []);
+  return properJsonOutput
 }
