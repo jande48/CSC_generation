f5541e4eb4200bf33dc7a5f49a730d622b8a0a56
Shawn Inder <shawninder@gmail.com>
2021-02-12 12:38:29 -0500

    Make a non-recursive version

--- sortCategoriesForInsert.js
+++ sortCategoriesForInsert.js
@@ -1,27 +1,26 @@
 module.exports = function sortCategoriesForInsert (inputJson) {
-  const input = JSON.parse(inputJson);
-  const hashArr = {};
-  for (let i = 0; i < input.length; i++) {
-    const item = input[i];
-    if (hashArr[item.parent_id] == null) hashArr[item.parent_id] = [];
-    hashArr[item.parent_id].push(item);
-  }
+  const inserted = {};
+  const waiting = {};
 
-  const properJsonOutput = hierarchySort(hashArr, null, []);
-
-  return JSON.stringify(properJsonOutput);
-}
-
-function hierarchySort (hashArr, key, result) {
-  // check to see if there are any children
-  if (hashArr[key] == undefined) return;
-
-  // separate out the array that have the same parents
-  const arrOfParents = hashArr[key];
-  for (let i = 0; i < arrOfParents.length; i++){
-    result.push(arrOfParents[i]);
-    hierarchySort(hashArr, arrOfParents[i]["id"], result);
-  }
-
-  return result;
+  return JSON.stringify(
+    JSON.parse(inputJson).reduce((output, item) => {
+      const parentId = item["parent_id"];
+      const id = item["id"];
+      if (parentId === null || inserted[parentId]) {
+        output.push(item);
+        inserted[id] = true;
+        const awaiting = waiting[id]
+        if (awaiting) {
+          output = output.concat(awaiting);
+          delete waiting[id];
+        }
+      } else {
+        if (!waiting[parentId]) {
+          waiting[parentId] = [];
+        }
+        waiting[parentId].push(item);
+      }
+      return output;
+    }, [])
+  );
 }
