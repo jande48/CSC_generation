2edefcb4ae0697b4175eef2b37e5caf194735891
Shawn Inder <shawninder@gmail.com>
2021-02-12 11:09:25 -0500

    use spacing consistently

--- sortCategoriesForInsert.js
+++ sortCategoriesForInsert.js
@@ -17,14 +17,14 @@ module.exports = function sortCategoriesForInsert (inputJson) {
   return properJsonOutput;
 }
 
-function hierarchySort(hashArr, key, result) {
+function hierarchySort (hashArr, key, result) {
 
   // check to see if there are any children
   if (hashArr[key] == undefined) return;
 
   // separate out the array that have the same parents
   var arrOfParents = hashArr[key];
-  for (var i=0; i<arrOfParents.length; i++){
+  for (var i = 0; i < arrOfParents.length; i++){
     result.push(arrOfParents[i]);
     hierarchySort(hashArr, arrOfParents[i]["id"], result);
   }
