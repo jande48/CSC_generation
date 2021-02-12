ec73734f3b9c9004b706554e2bb2eebd091da081
Shawn Inder <shawninder@gmail.com>
2021-02-12 11:10:44 -0500

    Use quotes consistently + fixed indentation

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
-var result = require('./sortCategoriesForInsert.js');
+var result = require("./sortCategoriesForInsert.js");
 
 var arr = [
+  {
+      "name": "Accessories",
+      "id": 1,
+      "parent_id": 20,
+    },
     {
-        "name": "Accessories",
-        "id": 1,
-        "parent_id": 20,
-      },
-      {
-        "name": "Watches",
-        "id": 57,
-        "parent_id": 1
-      },
-      {
-        "name": "Men",
-        "id": 20,
-        "parent_id": null,
-      },
-      {
-        "name": "Women",
-        "id": 22,
-        "parent_id": null,
-      },
-      {
-        "name": "Bags",
-        "id": 99,
-        "parent_id": 22,
-      },
-      {
-        "name": "Hand Bags",
-        "id": 31,
-        "parent_id": 99,
-      },
-      {
-        "name": "Doggy Bags",
-        "id": 32,
-        "parent_id": 99,
-      },
-      {
-        "name": "Purse Bags",
-        "id": 33,
-        "parent_id": 99,
-      },
-  ];
+      "name": "Watches",
+      "id": 57,
+      "parent_id": 1
+    },
+    {
+      "name": "Men",
+      "id": 20,
+      "parent_id": null,
+    },
+    {
+      "name": "Women",
+      "id": 22,
+      "parent_id": null,
+    },
+    {
+      "name": "Bags",
+      "id": 99,
+      "parent_id": 22,
+    },
+    {
+      "name": "Hand Bags",
+      "id": 31,
+      "parent_id": 99,
+    },
+    {
+      "name": "Doggy Bags",
+      "id": 32,
+      "parent_id": 99,
+    },
+    {
+      "name": "Purse Bags",
+      "id": 33,
+      "parent_id": 99,
+    },
+];
 
 console.log(result(arr));
