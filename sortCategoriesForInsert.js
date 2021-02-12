5a9d2af7f7e32ce35ca2b1d8087dba2cd8347542
Shawn Inder <shawninder@gmail.com>
2021-02-12 11:13:02 -0500

    remove information already present in the readme

--- sortCategoriesForInsert.js
+++ sortCategoriesForInsert.js
@@ -1,18 +1,12 @@
-// This function is a solution the problem at https://bitbucket.org/dbuy/workspace/snippets/rnB4an
-// developed by Jacob Anderson on 2/11/2020
-
-
 module.exports = function sortCategoriesForInsert (inputJson) {
 
   var arr = inputJson;
-  // Since this data is hierarchial, it seems that one way to efficiently solve it is to create a hash array that groups objects with the same parent id.
   var hashArr = {};
   for (var i = 0; i < arr.length; i++) {
       if (hashArr[arr[i].parent_id] == null) hashArr[arr[i].parent_id] = [];
       hashArr[arr[i].parent_id].push(arr[i]);
   }
 
-  // Once the data is grouped by parent, a recursive function is called to push all of the children to a 'result' array once their parents have aleady been added.
   var properJsonOutput = hierarchySort(hashArr, null, []);
   return properJsonOutput;
 }
